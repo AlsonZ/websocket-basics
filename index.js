0bd4c9114ad7612294123404a925ad5aedc4b7bd
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-08-31 15:34:28 +1000

    create messaging with websockets

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-const ws = new WebSocket('ws://localhost:8082');
-const button = document.getElementById('button');
+const sendButton = document.getElementById('sendButton');
+const messageText = document.getElementById('messageText');
+const messages = document.getElementById('messages');
+let ws;
 
-ws.addEventListener('open', () => {
-  console.log('we are connected');
+// ws.addEventListener('open', () => {
+//   console.log('we are connected');
 
-  ws.send('this is some data sent from frontend');
-})
+//   ws.send('this is some data sent from frontend');
+//   // enter room?
+//   // get history
+// })
 
-ws.addEventListener('message', (e) => {
-  console.log(e);
-});
+// ws.addEventListener('message', (e) => {
+//   console.log(e);
+//   //add message to messages
+// });
+const init = () => {
+  if(ws) {
+    ws.close();
+  }
+  ws = new WebSocket('ws://localhost:8082');
+  ws.onopen = () => {
+    console.log('Connection made!');
+  }
+  ws.onmessage = ({ data }) => showMessage(data);
+  ws.onclose = () => {
+    ws = null;
+  }
+}
+sendButton.onclick = () => {
+  if(!ws) {
+    showMessage('No Connection avaliable!');
+    return;
+  }
+  ws.send(messageText.value);
+  showMessage(messageText.value);
+}
+const showMessage = (message) => {
+  messages.textContent += `\r\n${message}`;
+  messageText.value = '';
+}
+
+init();
--- server/index.js
+++ server/index.js
@@ -1,18 +1,28 @@
 const WebSocket = require('ws');
 
-const socketServer = new WebSocket.Server({port: 8082});
+const wss = new WebSocket.Server({port: 8082});
 
-
-socketServer.on('connection', (socket) => {
-  console.log('new client connected');
-
-  socket.on('message', (data) => {
-    console.log('data from client:', data);
-
-    socket.send('hello this is a return message');
-  });
-
-  socket.on('close', () => {
-    console.log('client has disconnected');
+wss.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    //send data to all other clients
+    wss.clients.forEach((client) => {
+      if(client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    })
   })
-})
+});
+
+// socketServer.on('connection', (socket) => {
+//   console.log('new client connected');
+
+//   socket.on('message', (data) => {
+//     console.log('data from client:', data);
+
+//     socket.send('hello this is a return message');
+//   });
+
+//   socket.on('close', () => {
+//     console.log('client has disconnected');
+//   })
+// })

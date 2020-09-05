e5668c5a99d0c2cdeca341cbdfc95e8d28837160
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-09-05 20:53:12 +1000

    fix some styling issues

--- server/index.js
+++ server/index.js
@@ -6,56 +6,22 @@ const wss = new WebSocket.Server({port: 8082});
 const rooms = {}
 
 wss.on('connection', (ws, req) => {
-  // ws.id = url.parse(req,url, true);
-  // console.log(url.parse(req.url, true).query.username);
   ws.id = url.parse(req.url, true).query.username;
   ws.room = url.parse(req.url, true).query.room;
 
-  ws.isMsg = false;// might not need this
-
-  console.log(ws.id, ws.room);
-  // wss.clients.forEach((client) => {
-  //   const data = {
-  //     username: ws.id,
-  //   }
-  //   client.send(JSON.stringify(data));
-  // });
-  if(ws.isMsg) {
-    // sendMsgToUsers(msg.room, msg.author, msg.message);
-  } else {
-    if(rooms[`${ws.room}`]) {
-      //exists
-      addToRoom(ws);
-    } else if(!rooms[`${ws.room}`]) {
-      // make new room if doesnt exist
-      makeRoom(ws);
-    }
+  if(rooms[`${ws.room}`]) {
+    // exists
+    addToRoom(ws);
+  } else if(!rooms[`${ws.room}`]) {
+    // make new room if doesnt exist
+    makeRoom(ws);
   }
-  
-
   ws.on('message', (res) => {
-
     resData = JSON.parse(res);
     if(resData.isMsg) {
       console.log(ws.id, ws.room, resData.message);
       sendMsgToUsers(ws.room, ws.id, resData.message);
     }
-
-    //send data to all other clients
-    // wss.clients.forEach((client) => {
-      // console.log(client.id);
-      // if(client !== ws && client.readyState === WebSocket.OPEN) {
-      // if(client.readyState === WebSocket.OPEN) {
-      //   const data = {
-      //     author: ws.id,
-      //     message: message,
-      //   }
-      //   client.send(JSON.stringify(data));
-      // }
-    // });
-    // const msg = JSON.parse(message);
-    // check if room exists
-    
   });
 });
 
